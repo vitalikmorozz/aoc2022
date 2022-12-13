07ae5d56873ac1f0db2673c7725cc65f4779ed59
Vitalii Moroz <vitalik.moroz2@epam.com>
2022-12-13 16:11:56 +0200

    task 6

---
+++ 6/first.ts
@@ -0,0 +1,26 @@
+import { readFile } from "../shared/index.ts";
+
+const fileContent = await readFile('./input.txt');
+
+const prevFourCharacters: string[] = [];
+const chunkSize = 4;
+const characters = fileContent.trim().split('');
+
+let marker = 0;
+
+const isAllUnique = (arr: string[]): boolean => {
+    return arr.every((val, index) => arr.lastIndexOf(val) === index);
+}
+
+for (let i = 0; i < characters.length; i++) {
+    prevFourCharacters.push(characters[i]);
+    if (i >= chunkSize) {
+        prevFourCharacters.shift()
+        if (isAllUnique(prevFourCharacters)) {
+            marker = i + 1;
+            break;
+        }
+    }
+}
+
+console.log(marker);
