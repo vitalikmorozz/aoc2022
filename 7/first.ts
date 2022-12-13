c7867c199fc1f55d1632476f6c30782e3efc211d
Vitalii Moroz <vitalik.moroz2@epam.com>
2022-12-13 18:02:35 +0200

    task 7

---
+++ 7/first.ts
@@ -0,0 +1,79 @@
+import { readFile } from "../shared/index.ts";
+
+const fileContent = await readFile('./input.txt');
+
+type Node = {
+    type: 'dir' | 'file'
+    name: string;
+    parent?: Node;
+    children?: Map<string, Node>;
+    size: number;
+}
+
+const rootNode: Node = {
+    name: '/',
+    type: 'dir',
+    size: 0,
+    children: new Map(),
+    parent: undefined,
+}
+
+let currentNode: Node = rootNode;
+
+const recalculateParentSize = (node: Node, size: number) => {
+    node.size = size + (node.size || 0);
+    if (node.parent) {
+        recalculateParentSize(node.parent, size);
+    }
+}
+
+const printTree = (node: Node, depth = 0) => {
+    console.log(`${'-'.repeat(depth)} ${node.name} ${node.size ? `size: ${node.size}` : ''}`);
+    if (node.children) {
+        Array.from(node.children.values()).forEach(node => printTree(node, depth + 2));
+    }
+}
+
+const filterDirs = (node: Node, size: number): Node[] => {
+    if (node.type !== 'dir') return [];
+    const res: Node[] = [];
+    if (node.size < size) res.push(node);
+    if (node.children) {
+        res.push(...Array.from(node.children.values()).reduce((acc, curNode) => acc.concat(filterDirs(curNode, size) || []), [] as Node[]));
+    }
+    return res;
+}
+
+fileContent.trim().split('\n').forEach((command) => {
+    if (command.match(/\$ cd/)) {
+        const changeTo = command.slice(5);
+        if (changeTo === '/') return currentNode = rootNode;
+        if (changeTo === '..') return currentNode = currentNode.parent!;
+        return currentNode = currentNode.children?.get(changeTo)!;
+    }
+    if (command.match(/\$ ls/)) return currentNode.children = new Map();
+    
+    const [dirOrSize, name] = command.split(' ');
+
+    if (dirOrSize === 'dir') return currentNode.children?.set(name, {
+        type: 'dir',
+        parent: currentNode,
+        size: 0,
+        name,
+    });
+
+    currentNode.children?.set(name, {
+        type: 'file',
+        parent: currentNode,
+        size: parseInt(dirOrSize),
+        name,
+    });
+
+    recalculateParentSize(currentNode, parseInt(dirOrSize));
+})
+
+// printTree(rootNode);
+
+const result = filterDirs(rootNode, 100000).reduce((sum, node) => sum + node.size, 0);
+
+console.log(result);
